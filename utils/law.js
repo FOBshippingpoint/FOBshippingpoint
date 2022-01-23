625d993c477b3b8f9fe9ded1bf62ab9d1ce78dfa
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2022-01-23 20:17:22 +0800

    exclamationer

--- utils/law.js
+++
@@ -1,8 +0,0 @@
-function lawProcessor(chLaw) {
-  // 各法律
-  const laws = chLaw.Laws;
-  laws.forEach((l) => {
-    const articles = l.LawArticles;
-    articles.forEach((a) => (a.ArticleNo = v.substr(a.ArticleNo, 2, -3)));
-  });
-}
